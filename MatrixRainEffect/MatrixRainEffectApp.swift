7874b3ad5041bc6bee1034ebb2be76902ca94290
蘇健豪 <jenhausu@osensetech.com>
2022-02-15 14:59:14 +0800

    Initial Commit

---
+++ MatrixRainEffect/MatrixRainEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatrixRainEffectApp.swift
+//  MatrixRainEffect
+//
+//  Created by 蘇健豪 on 2022/2/15.
+//
+
+import SwiftUI
+
+@main
+struct MatrixRainEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
